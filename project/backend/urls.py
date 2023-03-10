6bb5ff8aa5e785b5e9a15916452254e88d2de621
Perevyazko <a.perevyazko@gmail.com>
2023-03-10 19:40:29 +0300

    refactoring

--- project/backend/urls.py
+++ project/backend/urls.py
@@ -1,6 +1,5 @@
 from django.urls import include, path
 from rest_framework.authtoken.views import obtain_auth_token
-from rest_framework.routers import DefaultRouter
 
 from .views import UserProfileListCreateView, userProfileDetailView, \
     RoomViewSet, MessageViewSet, Index
