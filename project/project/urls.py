bc0bafb71f4a0723623d83e3e5e1779f7f4135b1
Perevyazko <a.perevyazko@gmail.com>
2023-03-14 12:04:35 +0300

    finish

--- project/project/urls.py
+++ project/project/urls.py
@@ -22,6 +22,6 @@ urlpatterns = [
     # path('auth/', include('djoser.urls')),
     # path('auth/', include('djoser.urls.jwt')),
     path("api/", include("backend.urls")),
-    # path("", include("chat.urls")),
+    path("", include("chat.urls")),
 
 ]
